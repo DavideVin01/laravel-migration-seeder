59b2ff0646ede7e3197c3eedd747adfa7463f74c
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-03-20 18:49:57 +0100

    Creates trains table + tables

---
+++ database/migrations/2022_03_20_173037_create_trains_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda');
+            $table->char('Stazione di partenza', 1);
+            $table->char('Stazione di arrivo', 1);
+            $table->char('Codice Treno Arrivo', 5);
+            $table->char('Carrozze', 2);
+            $table->boolean('In arrivo');
+            $table->boolean('Cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
